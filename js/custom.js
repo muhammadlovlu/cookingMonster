9227ed0032e452f1d1ece6a9ba30a6852a3c5589
muhammadlovlu <chyshahab@gmail.com>
2021-02-20 22:31:43 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -2,27 +2,29 @@
 // .then (res => res.json())
 // .then (data => console.log(data))
 
-
-
-
 document.getElementById("search").addEventListener('click', function () {
+    let Result = document.getElementById("result");
+    Result.innerHTML = "";
     search()
-    .catch(error => alert("Type First Letter Of Your Favourite Food !"))
+        .catch(error => alert("Type First Letter Of Your Favourite Food !"))
 })
 
+
 async function search() {
-    const input = document.getElementById("input-Text").value;
+    let input = document.getElementById("input-Text").value;
     const api = `https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`
     const res = await fetch(api)
     const data = await res.json()
     showFoodItems(data.meals)
-   
 }
 
 
 const showFoodItems = items => {
-
-    const Result = document.getElementById("result");
+    const welcomeSection = document.getElementById("welcome");
+    welcomeSection.style.display = "none";
+    const fullBox = document.getElementById("full-box");
+    fullBox.style.minHeight = "937px";
+    Result = document.getElementById("result");
     items.forEach(item => {
         const Div = document.createElement("div");
         Div.className = 'box';
@@ -32,10 +34,6 @@ const showFoodItems = items => {
         `;
         Div.innerHTML = itemsAll;
         Result.appendChild(Div)
-
-       
-     
-
     })
 }
 
