9d8682d1c6a71f388b17e173bcace6296c2d83eb
muhammadlovlu <chyshahab@gmail.com>
2021-02-20 21:04:46 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -7,31 +7,34 @@
 
 document.getElementById("search").addEventListener('click', function () {
     search()
+    .catch(error => alert("Type First Letter Of Your Favourite Food !"))
 })
 
 async function search() {
-    const inputValue = document.getElementById("input-Text").value;
-    const api = `https://www.themealdb.com/api/json/v1/1/search.php?f=${inputValue}`
+    const input = document.getElementById("input-Text").value;
+    const api = `https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`
     const res = await fetch(api)
     const data = await res.json()
-
     showFoodItems(data.meals)
-
+   
 }
 
 
 const showFoodItems = items => {
+
     const Result = document.getElementById("result");
     items.forEach(item => {
         const Div = document.createElement("div");
         Div.className = 'box';
         const itemsAll = `
-        <img src="${item.strMealThumb}" />
-        <h2>${item.strMeal}</h2>
+        <img class="image w-100" src="${item.strMealThumb}" />
+        <h2 class="headline">${item.strMeal}</h2>
         `;
         Div.innerHTML = itemsAll;
         Result.appendChild(Div)
-        console.log(Div)
+
+       
+     
 
     })
 }
